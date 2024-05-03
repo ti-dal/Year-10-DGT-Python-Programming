7fb6ad142917dd2d9d4f88e14646660b73083e45
ti-dal <168596271+ti-dal@users.noreply.github.com>
2024-05-03 14:33:14 +1200

    added variable work
    used videos instead of live teaching

---
+++ variables.py
@@ -0,0 +1,14 @@
+# Showing how variables work
+# Harrison May
+# 3 Mary 2024
+
+# this was done with video tutorials instead of live teaching
+sum = "joe" + "3"
+print(sum)
+
+fav_num = 7
+
+print(fav_num)
+
+PI = 3.14159
+print(PI)
