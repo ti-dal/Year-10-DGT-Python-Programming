bd74e44bad3e04a8ffd8bb0fda6897d528c0a58a
ti-dal <168596271+ti-dal@users.noreply.github.com>
2024-05-08 10:02:40 +1200

    Added functions program
    learnt 8/05/2024

---
+++ functions.py
@@ -0,0 +1,10 @@
+#functions go here
+def intro():
+    print("Welcome")
+    print()
+    print("Instructions go here")
+    return "" #I am returning all items in the code
+                # in the code
+
+# Main routine
+intro() #This calls the function so that we cna use it
