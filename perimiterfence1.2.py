3995682c5b9c58b19d22a18f5896f95b13221066
ti-dal <168596271+ti-dal@users.noreply.github.com>
2024-06-05 14:46:55 +1200

    Updated Perimeter Fence Calculator to version 1.2
    Change log:
    -Added a thank you message for using the calculator if the user quits the program
    -Bug fixed a small bug
    -Made the input for quitting the program slightly cleaner

---
+++ perimiterfence1.2.py
@@ -0,0 +1,42 @@
+# Author: Harrison May
+# Date: 22/05/2024
+# Version: 1.2
+
+# Ends the programme
+finished = False
+# The loop to let the code run indefinintely until asked to stop
+while not finished:
+
+    try:
+        # Defining width, length and cost
+        
+        width = int(input("Width: "))
+        length = int(input("Length: "))
+        cost = int(input("Cost: "))
+        
+       
+       # The code to get the perimeter and cost of the fence
+        if width > 0 and length > 0 and cost > 0:
+            # Adding the perimeter
+            perimeter = (width+length)*2
+            print(f"perimeter = {perimeter}m") # Displays the perimeter with "m" after the value for the unit of measurement
+            cost = perimeter*cost
+            cost = str(cost)
+            print("$" + cost)
+            # Asking the user if they want to continue
+            end_program = input(str("Press Enter to continue or press q to quit. "))
+            if end_program == "q":
+                print ("Thank you for using the perimiter calculator. ")
+                finished = True
+            else:
+                print("restart")
+            
+        # If the user inputs an invalid number display "Invalid value"
+        else:
+            print("Invalid value")
+
+    # If the user inputs an invalid character (excluding numbers) display "Invalid input"
+    except ValueError:
+        print("Invalid input")
+
+
